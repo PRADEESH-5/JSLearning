f26ac3e763aa73a1aa034188844534deae2e3d86
PRADEESH-5 <enochpradeesh@gmail.com>
2026-03-03 20:48:09 +0530

    Daily update 03/03/2026 20:48 ~

---
+++ TriviaBot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Gemini";
+let botLocation = "the cloud";
+let favoriteLanguage = "C#";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `Did you know that ${favoriteLanguage} was originally going to be named "Cool" (C-like Object Oriented Language)?`;
+console.log(codingFact);
+
+codingFact = `Another fun fact: The name "${favoriteLanguage}" was inspired by musical notation, where a sharp indicates that a written note should be made a semitone higher in pitch!`;
+console.log(codingFact);
+
+codingFact = `One last fact: ${favoriteLanguage} was designed by Anders Hejlsberg, the same brilliant engineer who later created TypeScript.`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
