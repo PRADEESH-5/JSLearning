d22e8bb0de067e3c2689204bd634383ddc3872bc
PRADEESH-5 <enochpradeesh@gmail.com>
2026-03-03 09:51:58 +0530

    Daily update 03/03/2026 09:51 ~

---
+++ Arrays.js
@@ -0,0 +1,24 @@
+const a=[1,3,2,4,5,6,5,4];
+
+console.log(a.length)
+
+s=a.toString()
+console.log(s);
+
+console.log(a.at(0))
+
+j=a.join("*")
+console.log(j)
+
+a.pop()
+console.log(a)
+
+a.push(9)
+console.log(a)
+
+a.shift()
+console.log(a)
+
+a.unshift(67)
+console.log(a)
+
